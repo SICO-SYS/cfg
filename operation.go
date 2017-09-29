5d1f797bb40a4e1ab3ca4daec2730ece4d93157a
SingleCorner <sinerwr@gmail.com>
2017-09-29 21:44:56 +0800

    1.0.2

---
+++ operation.go
@@ -0,0 +1,60 @@
+/*
+
+LICENSE:  MIT
+Author:   sine
+Email:    sinerwr@gmail.com
+
+Contributors
+
+*/
+
+package cfg
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/SiCo-Ops/Pb"
+	"github.com/SiCo-Ops/dao/grpc"
+)
+
+func ReadLocalFile() []byte {
+	data, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		return nil
+	}
+	return data
+}
+
+func ReadFilePath(path string) ([]byte, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func ReadConfigServer() []byte {
+	cc, err := rpc.Conn("B", "6666")
+	if err != nil {
+		return nil
+	}
+	defer cc.Close()
+	r := rpc.ConfigPullRPC(cc, &pb.ConfigPullCall{Id: "system", Environment: "config"})
+	return r.Params
+}
+
+func Map2struct(m map[string]string, c *ConfigItems) {
+	data, _ := json.Marshal(m)
+	err := json.Unmarshal(data, c)
+	if err != nil {
+		c = &ConfigItems{}
+	}
+}
+
+func Unmarshal(data []byte, c *ConfigItems) {
+	err := json.Unmarshal(data, c)
+	if err != nil {
+		c = &ConfigItems{}
+	}
+}
